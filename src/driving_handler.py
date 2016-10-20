b399c736434a2fd9e5572b0e23a9745075c176a9
Michael Pollind <mpollind@gmail.com>
2016-10-19 20:54:44 -0700

    start

---
+++ src/driving_handler.py
@@ -0,0 +1,26 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import gzip
+import os
+import re
+import sys
+import tarfile
+
+from six.moves import urllib
+import tensorflow as tf
+
+class driving_handler():
+	"""docstring for car_network"""
+	def __init__(self, arg):
+		super(car_network, self).__init__()
+		self.arg = arg
+
+	def model(self):
+		pass
+
+	def train(self):
+		pass
+
+		
